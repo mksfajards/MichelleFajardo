571242132330af63cf0c68157411613ea3738454
mksfajards <mksfajards@gmail.com>
2023-02-01 13:53:28 -0500

    jsupdated

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Aspiring Software Eng", "Software Developer", "Web Editor", "Photographer", "Designer", "Freelancer"],
+        strings: ["Aspiring Software Eng.", "Software Developer", "Web Editor", "Photographer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Aspiring Software Eng", "Software Developer", "Web Editor", "Photographer", "Designer", "Freelancer"],
+        strings: ["Aspiring Software Eng.", "Software Developer", "Web Editor", "Photographer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
